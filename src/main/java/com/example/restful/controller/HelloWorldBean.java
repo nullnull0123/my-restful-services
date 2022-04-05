b7b947b6b2e5a3a7eaa43cef125f187676f16727
nullnull0123 <kimty93123@gmail.com>
2022-04-05 15:56:08 +0900

    feat: HelloWorld Bean 추가

---
+++ src/main/java/com/example/restful/controller/HelloWorldBean.java
@@ -0,0 +1,13 @@
+package com.example.restful.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class HelloWorldBean {
+
+    private String message;
+}
