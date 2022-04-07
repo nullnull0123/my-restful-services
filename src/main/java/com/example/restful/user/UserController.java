f39cfd6dbb49d50235c4609685226f4b1fd1954d
nullnull0123 <kimty93123@gmail.com>
2022-04-07 21:47:33 +0900

    feat: 사용자 등록 api 구현

--- src/main/java/com/example/restful/user/UserController.java
+++ src/main/java/com/example/restful/user/UserController.java
@@ -1,8 +1,6 @@
 package com.example.restful.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +20,10 @@ public class UserController {
     @GetMapping("/users/{id}")
     public User retrieveAllUsers(@PathVariable int id) {
         return service.findOne(id);
+    }
+
+    @PostMapping(path = "/users")
+    public void createUser(@RequestBody User user){
+        User savedUser = service.save(user);
     }
 }
