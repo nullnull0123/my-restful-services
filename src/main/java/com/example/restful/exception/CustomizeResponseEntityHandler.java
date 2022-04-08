b3c9ee8fd0a08536ed5389bfd028685f5523f3f0
nullnull0123 <kimty93123@gmail.com>
2022-04-08 13:39:16 +0900

    feat: AOP를 이용한 Exception Handling

---
+++ src/main/java/com/example/restful/exception/CustomizeResponseEntityHandler.java
@@ -0,0 +1,35 @@
+package com.example.restful.exception;
+
+import com.example.restful.user.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@RestController
+@ControllerAdvice
+public class CustomizeResponseEntityHandler extends ResponseEntityExceptionHandler {
+
+    // 모든 예외처리를 전부 다 처리해줌
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // 특정 예외 코드가 발생할 때마다 반드시 실행되는 메소드
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<Object> handleUserNotFoundExceptionExceptions(Exception ex, WebRequest request) {
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+}
