148b8cd9f2a5437a659ccc53f2a015025f66ba52
nullnull0123 <kimty93123@gmail.com>
2022-04-11 15:19:49 +0900

    feat: response 데이터 제어

--- src/main/java/com/example/restful/user/User.java
+++ src/main/java/com/example/restful/user/User.java
@@ -1,5 +1,7 @@
 package com.example.restful.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,10 +11,16 @@ import java.util.Date;
 
 @Data
 @AllArgsConstructor
+@JsonIgnoreProperties(value={"password", "ssn"})
 public class User {
     private Integer id;
     @Size(min=2, message = "name은 2글자 이상 입력해 주세요")
     private String name;
     @Past
     private Date joinDate;
+
+    //@JsonIgnore
+    private String password;
+    //@JsonIgnore
+    private String ssn;
 }
